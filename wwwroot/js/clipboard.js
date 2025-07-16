bca7ed669de9fc724e52cb89f663aeb82890f7a9
kawa0x0A <10515785+kawa0x0A@users.noreply.github.com>
2025-07-16 12:14:21 +0900

    Update clipboard.js

--- wwwroot/js/clipboard.js
+++ wwwroot/js/clipboard.js
@@ -5,3 +5,11 @@
         console.error(error.message);
     }
 }
+
+export async function pasteText(text) {
+    try {
+        return await navigator.clipboard.readText();
+    } catch (error) {
+        console.error(error.message);
+    }
+}
